c32469e55bb34f657320bd914f2d666513d6cf15
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-10-21 18:06:52 -0700

    created recipe dummy data file and project folder structure

---
+++ src/RecipeData.js
@@ -0,0 +1,54 @@
+const RecipeData = {
+    "projects": [
+        {
+            "id": 1,
+            "title": "Nachos",
+            "description": "Traditional nachos w/ Ground Beef",
+            "category": "Appetizer",
+            "img": "nachos.jpg",
+            "yield": "3-4 servings",
+            "time": "20-30 minutes",
+            "ingredients": "1 bag of tortilla chips,1 lb ground beef,2 c Shredded cheese",
+            "instructions": "Preheat oven to 350,add chips in an even layer,bake for 10 minutes until cheese is melted and burger is heated through",
+            "notes": "additional comments go here"
+        },
+        {
+            "id": 2,
+            "title": "Tacos",
+            "description": "Baja Tacos w/ Shrimp",
+            "category": "Main",
+            "img": "tacos.jpg",
+            "yield": "6-8 tacos",
+            "time": "1 hr",
+            "ingredients": "6 taco shells,1 lb shrimp,2 c Shredded cheese, 4 c cabbage",
+            "instructions": "Preheat oven to 350,line taco shells with shredded cheese and bake for 14 minutes or according to brand instructions,throw shrimp away- they're terrible",
+            "notes": "additional comments go here"
+        },      
+        {
+            "id": 3,
+            "title": "Burritos",
+            "description": "Chicken wet burritos w/ mole",
+            "category": "Main",
+            "img": "burritos.jpg",
+            "yield": "4 Burritos",
+            "time": "15 minutes",
+            "ingredients": "6 flour tortilla shells,1 lb chicken,2 c Shredded cheese, 4 c cabbage, 1 can black beans",
+            "instructions": "Cook chicken in pan until cooked through-approximately 6 minutes per side,remove from pan and cut into strips,make burrito",
+            "notes": "additional comments go here"
+        },  
+        {
+            "id": 4,
+            "title": "Churros",
+            "description": "Churros w/ custard dipping sauce",
+            "category": "Dessert",
+            "img": "churros.jpg",
+            "yield": "12 churros",
+            "time": "1.5 hrs",
+            "ingredients": "2 c flour,1 tsp baking soda,1/4 c sugar, 1 1/2 tbsp cinnamon",
+            "instructions": "Heat oil in fryer to temperature,add churros to oil and cook for some amount of time",
+            "notes": "additional comments go here"
+        }  
+    ]
+
+}
+export default ProjectData;
