87dc855cb62df3390e3886a4e3c931722e69755c
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-12-07 14:35:16 -0800

    added bcrypt and jsontoken to user routes

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.Port || 5000;
-const db = require("./models");
-var bodyParser = require("body-parser");
+const db = require('./models');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const HttpError = require('./server_models/http-error');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((error, req,res,next) =>{
+    if(res.headerSent){
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message: error.message || "An unknown error occurred."});
+})
+
 
 // create a GET route
 app.get('/express_backend', (req,res) => {
@@ -19,12 +30,23 @@ db.sequelize.sync().then(function() {
     });
 });
 
+// ---------- USER ROUTES ----------//
 // SIGNUP route for new accounts
 app.post('/signup', (req,res) => {
+    const {email, password, first, last} = req.body;
+
+    let hashedPassword;
+    try {
+       bcrypt.hash(password, 12).then(function(res){hashedPassword=res}); 
+    } catch (err) {
+        const error = new HttpError('Could not create user. Please try again', 500);
+        return next(error);
+    }
+    
     db.Users.findOrCreate({
         where: {
             email:email,
-            password: password,
+            password: hashedPassword,
             first: first,
             last: last
         }
@@ -32,16 +54,59 @@ app.post('/signup', (req,res) => {
         if (!created){
             res.send('Something went wrong');
             res.end();
+        } else {
+            let token;
+            try{
+                token = jwt.sign(
+                    {userId: user.id, email: user.email}, 
+                    "I'm making curry chicken pitas for lunch", 
+                    {expiresIn: '1hr'}
+                );    
+            } catch (err){
+                const error = new HttpError('Signin failed. Please try again', 500);
+                return next(error);
+            }
+
+            res
+            .status(200)
+            .json({userId: user.id, email: user.email, token: token})           
         }
     })
 })
 
 // LOG IN route for users to log in
 app.get('/login', (req,res) =>{
+    const {email, password} = req.body;
+
     db.Users.findOne({
-        where: {email: email, password: password}
+        where: {email: email}
     }).then(user => {
-        res.send({user});
+        let isValidPassword = false;
+        try {
+           bcrypt.compare(password, user.password).then(function(res){isValidPassword = res}); 
+        } catch (err) {
+            const error = new HttpError("Could not log you in. Please check your credentials and try again", 500);
+            return next(error);
+        }
+
+        if(!isValidPassword){
+            console.log("not the right credentials, bro");
+        } else {
+            let token;
+            try {
+                token = jwt.sign(
+                    {user: user.id, email: user.email},
+                    "I'm making curry chicken pitas for lunch", 
+                    {expiresIn: '1hr'}
+                )
+            } catch (err) {
+                const error = new HttpError("Server error while logging in. Please try again", 500);
+                return next(error)
+            }
+            res
+            .status(200)
+            .json({userId: user.id, email: user.email, token: token});
+        }
     })
 })
 
@@ -54,14 +119,13 @@ app.get('/logout', (req,res) =>{
       })
 })
 
+// ---------- RECIPE ROUTES ----------//
 // GET RECIPES route for recipebox
 app.get('/recipebox', (req,res) => {
     db.Recipe.findAll().then(recipes=>{
         res.send({recipes});
     })
 })
-
-
 
 //should submit-recipe to database//
 app.post('/submit-recipe', (req, res) => {
