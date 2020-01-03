9c5fc6eb9553c56d0480003f6c04975dbce28b6a
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-01-02 16:16:29 -0800

    Added conditional rendering to nav links based on auth

--- client/src/components/NavLinks/NavLinks.js
+++ client/src/components/NavLinks/NavLinks.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from 'react-router-dom';
+
+import AuthContext from "../../shared/auth-context"
 
 import "./NavLinks.css"
 
 const NavLinks = props => {
+    const auth = useContext(AuthContext);
+
     return (
         <nav id="mainNav">
-            <ul>
-                <li>
-                    <NavLink to="/" exact>Recipe Box</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/new-recipe" exact>Add Recipe</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/search-recipes" exact>Search Recipes</NavLink>
-                </li>
-                <li>
-                    Log Out
-                </li>
-            </ul>
+            {auth.isLoggedIn && (
+                <ul>
+                    <li>
+                        <NavLink to="/" exact>Recipe Box</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/new-recipe" exact>Add Recipe</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/search-recipes" exact>Search Recipes</NavLink>
+                    </li>
+                    <li>
+                        Log Out
+                    </li>
+                </ul>                
+            )}
         </nav>
     )
 };
