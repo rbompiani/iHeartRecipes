114585f4c08ce580ba3016fe107ac248eecb9ba2
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-01-03 12:54:57 -0800

    added modal to recipe card click- needs styling and info

--- client/src/components/RecipeCard/RecipeCard.js
+++ client/src/components/RecipeCard/RecipeCard.js
@@ -1,21 +1,37 @@
-import React from "react";
-import "./RecipeCard.css";
-import Heart from "../Heart/Heart";
+import React, { useState } from "react";
 
-const RecipeCard = props => (
-    <div className="recipeCard">
-        <img src={props.img} className="cardThumbnail"/>
-        <div className="cardInfoWrapper">
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
-            <div className="cardFooter">
-                <span className="category">{props.category}</span>
-                <span className="time">{props.time}</span>
-                <Heart isFav={props.isFav} item={props.id} click={props.heartClick}/>       
-            </div>
-        </div>
-        
-    </div>
-);
+import Heart from "../Heart/Heart";
+import Modal from '../../shared/Modal/Modal';
+
+import "./RecipeCard.css";
+
+const RecipeCard = props => {
+
+    const [showRecipe, setShowRecipe] = useState(false);
+    const openRecipeHandler = () => setShowRecipe(true);
+    const closeRecipeHandler = () => setShowRecipe(false);
+
+    return (
+        <React.Fragment>
+            <Modal show={showRecipe} onCancel={closeRecipeHandler} header={props.title} >
+                <div>{props.description}</div>
+            </Modal>
+            <div className="recipeCard" onClick={openRecipeHandler}>
+                <img src={props.img} className="cardThumbnail"/>
+                <div className="cardInfoWrapper">
+                    <h2>{props.title}</h2>
+                    <p>{props.description}</p>
+                    <div className="cardFooter">
+                        <span className="category">{props.category}</span>
+                        <span className="time">{props.time}</span>
+                        <Heart isFav={props.isFav} item={props.id} click={props.heartClick}/>       
+                    </div>
+                </div>
+                
+            </div>             
+        </React.Fragment>
+    );
+
+};
 
 export default RecipeCard;
