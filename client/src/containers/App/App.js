86b9f97368dddd12a9f04b153a67a935a762d2d0
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-01-04 12:50:18 -0800

    updated app state to include user id

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,80 +1,96 @@
 import React, { Component, useCallback } from 'react';
 import './App.css';
-import {BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
-import Header from "../../components/Header/Header";
-import RecipeBox from "../RecipeBox/RecipeBox";
-import RecipeForm from "../RecipeForm/RecipeForm";
-import UserAuthForm from "../UserAuthForm/UserAuthForm";
-import AuthContext from "../../shared/auth-context";
-
-
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+import RecipeBox from '../RecipeBox/RecipeBox';
+import RecipeForm from '../RecipeForm/RecipeForm';
+import UserAuthForm from '../UserAuthForm/UserAuthForm';
+import AuthContext from '../../shared/auth-context';
 
 class App extends Component {
-  state = {
-    data: null,
-    isLoggedIn: false,
-    token: null
-  };
+	state = {
+		data: null,
+		isLoggedIn: false,
+		token: null,
+		userId: null
+	};
 
-  componentDidMount() {
-      // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
+	componentDidMount() {
+		// Call our fetch function below once the component mounts
+		this.callBackendAPI()
+			.then(res => this.setState({ data: res.express }))
+			.catch(err => console.log(err));
+	}
+	// Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+	callBackendAPI = async () => {
+		const response = await fetch('/express_backend');
+		const body = await response.json();
 
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
+		if (response.status !== 200) {
+			throw Error(body.message);
+		}
+		return body;
+	};
 
-  render() {
+	render() {
+		const login = (token, uid) =>
+			this.setState({
+				isLoggedIn: true,
+				token: token,
+				userId: uid
+			});
 
-    const login = (token) => this.setState({isLoggedIn: true, token: token});
-    const logout = () => this.setState({isLoggedIn: false, token: null});
+		const logout = () =>
+			this.setState({
+				isLoggedIn: false,
+				token: null,
+				userId: null
+			});
 
-    let routes;
+		let routes;
 
-    if (this.state.isLoggedIn){
-      routes = (
-        <Switch >
-          <Route path="/" exact >
-            <RecipeBox />
-          </Route>
+		if (this.state.isLoggedIn) {
+			routes = (
+				<Switch>
+					<Route path="/" exact>
+						<RecipeBox />
+					</Route>
 
-          <Route path="/new-recipe" exact >
-            <RecipeForm /> 
-          </Route>
+					<Route path="/new-recipe" exact>
+						<RecipeForm />
+					</Route>
 
-          <Redirect to="/" />         
-        </Switch>
-      );
-    } else {
-      routes = (
-        <Switch>
-          <Route path="/auth" exact>
-            <UserAuthForm />
-          </Route>
-          <Redirect to="/auth" />     
-        </Switch>
-      );
-    }
+					<Redirect to="/" />
+				</Switch>
+			);
+		} else {
+			routes = (
+				<Switch>
+					<Route path="/auth" exact>
+						<UserAuthForm />
+					</Route>
+					<Redirect to="/auth" />
+				</Switch>
+			);
+		}
 
-    return (
-      <AuthContext.Provider value={ {isLoggedIn : this.state.isLoggedIn, token: this.state.token, login : login, logout : logout} }>
-        <BrowserRouter>
-          <Header />
-          {routes}  
-        </BrowserRouter>        
-      </AuthContext.Provider>
-
-    );
-  }
+		return (
+			<AuthContext.Provider
+				value={{
+					isLoggedIn: this.state.isLoggedIn,
+					token: this.state.token,
+					userID: this.state.userId,
+					login: login,
+					logout: logout
+				}}
+			>
+				<BrowserRouter>
+					<Header />
+					{routes}
+				</BrowserRouter>
+			</AuthContext.Provider>
+		);
+	}
 }
 
 export default App;
