5fb4601adec597742a04d9bfc78c356223091519
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-01-04 16:06:06 -0800

    added logout timer set to 1hr

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,31 +1,43 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
 import Header from '../../components/Header/Header';
 import RecipeBox from '../RecipeBox/RecipeBox';
 import RecipeForm from '../RecipeForm/RecipeForm';
 import UserAuthForm from '../UserAuthForm/UserAuthForm';
 import AuthContext from '../../shared/auth-context';
 
+import './App.css';
+
+let logoutTimer;
+
 class App extends Component {
 	state = {
 		data: null,
 		isLoggedIn: false,
 		token: null,
-		userId: null
+		userId: null,
+		tokenExpiration: null
 	};
 
-	login = (token, uid) => {
+	login = (token, uid, expirationDate) => {
+		const tokenExpirationDate =
+			expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+
 		this.setState({
 			isLoggedIn: true,
 			token: token,
-			userId: uid
+			userId: uid,
+			tokenExpiration: tokenExpirationDate
 		});
 
-		const tokenExpirationDate = new Date(new Date().getTime() + 1000 * 60 * 60);
 		localStorage.setItem(
 			'userData',
-			JSON.stringify({ userId: uid, token: token })
+			JSON.stringify({
+				userId: uid,
+				token: token,
+				expiration: tokenExpirationDate.toISOString()
+			})
 		);
 	};
 
@@ -33,7 +45,8 @@ class App extends Component {
 		this.setState({
 			isLoggedIn: false,
 			token: null,
-			userId: null
+			userId: null,
+			tokenExpiration: null
 		});
 		localStorage.removeItem('userData');
 	};
@@ -42,8 +55,16 @@ class App extends Component {
 		// check local storage for user id
 		const storedUserData = JSON.parse(localStorage.getItem('userData'));
 		console.log(storedUserData);
-		if (storedUserData && storedUserData.token) {
-			this.login(storedUserData.token, storedUserData.userId);
+		if (
+			storedUserData &&
+			storedUserData.token &&
+			new Date(storedUserData.expiration) > new Date()
+		) {
+			this.login(
+				storedUserData.token,
+				storedUserData.userId,
+				new Date(storedUserData.expiration)
+			);
 		}
 
 		// Call our fetch function below once the component mounts
@@ -63,6 +84,14 @@ class App extends Component {
 	};
 
 	render() {
+		if (this.state.token && this.state.tokenExpiration) {
+			const remainingTime =
+				this.state.tokenExpiration.getTime() - new Date().getTime();
+			logoutTimer = setTimeout(this.logout, remainingTime);
+		} else {
+			clearTimeout(logoutTimer);
+		}
+
 		let routes;
 
 		if (this.state.isLoggedIn) {
